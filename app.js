cd6170b6e29af91b9eff6c7b4a9285560e66081a
CapCherif <gharbi_cherif@hotmail.com>
2025-01-09 09:30:10 +0100

    ajout d'une fonction Example

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 fetch('https://jsonplaceholder.typicode.com/posts/1')
   .then((response) => response.json())
   .then((json) => console.log(json));
+
+
+  function Example(){
+    console.log("example")
+  }
